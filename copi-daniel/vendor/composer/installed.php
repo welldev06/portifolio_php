5a25b9e6381d5473f4f4bf155e7a1f2ac3355077
welldev06 <wellingtongamer146@gmail.com>
2025-05-05 14:14:42 -0300

    Inicio.

---
+++ copi-daniel/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wlima/copi-daniel',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'projectmit/php-analyzes-execute-file-format' => array(
+            'pretty_version' => 'v0.1.0',
+            'version' => '0.1.0.0',
+            'reference' => 'bac1c43178abe71994abd24fcc51259738add7dc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../projectmit/php-analyzes-execute-file-format',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+        'wlima/copi-daniel' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
